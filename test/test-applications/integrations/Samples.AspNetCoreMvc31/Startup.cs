a89e8f12574d8ae0b74a49372ff9b7c07070a736
Andrew Lock <andrew.lock@datadoghq.com>
2021-06-16 15:57:33 +0100

    Update 3.0 and 3.1 apps with custom logger work

--- test/test-applications/integrations/Samples.AspNetCoreMvc31/Startup.cs
+++ test/test-applications/integrations/Samples.AspNetCoreMvc31/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Samples.AspNetCoreMvc
 {
@@ -22,12 +23,15 @@ namespace Samples.AspNetCoreMvc
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> startupLog)
         {
+            LogHelpers.WriteUnTracedLog(startupLog);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMiddleware<LogHelpers.LogMiddleware>();
 
             app.UseRouting();
 
