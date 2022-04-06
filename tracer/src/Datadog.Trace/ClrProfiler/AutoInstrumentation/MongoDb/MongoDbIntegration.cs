b9775c1a2383e222bdb23731cb339a362874d8ed
moonheart <themoonheart@outlook.com>
2022-04-06 11:01:25 +0800

    add mongodb obfuscation

--- tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MongoDb/MongoDbIntegration.cs
+++ tracer/src/Datadog.Trace/ClrProfiler/AutoInstrumentation/MongoDb/MongoDbIntegration.cs
@@ -4,7 +4,9 @@
 // </copyright>
 
 using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Text;
 using Datadog.Trace.Configuration;
 using Datadog.Trace.DuckTyping;
 using Datadog.Trace.Logging;
@@ -58,7 +60,7 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MongoDb
             }
 
             if (wireProtocol.TryDuckCast<IWireProtocolWithCommandStruct>(out var protocolWithCommand)
-                && protocolWithCommand.Command != null)
+             && protocolWithCommand.Command != null)
             {
                 try
                 {
@@ -72,7 +74,8 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MongoDb
                         return null;
                     }
 
-                    resourceName = $"{operationName ?? "operation"} {databaseName ?? "database"}";
+                    // resourceName = $"{operationName ?? "operation"} {databaseName ?? "database"}";
+                    resourceName = JsonObfuscator.FromJson(protocolWithCommand.Command.ToString());
                     collectionName = firstElement.Value?.ToString();
                     query = protocolWithCommand.Command.ToString();
                 }
@@ -147,6 +150,193 @@ namespace Datadog.Trace.ClrProfiler.AutoInstrumentation.MongoDb
             }
 
             return null;
+        }
+
+        private static class JsonObfuscator
+        {
+            [ThreadStatic]
+            private static StringBuilder _stringBuilder;
+
+            [ThreadStatic]
+            private static StringBuilder _stringBuilderRes;
+
+            public static string FromJson(string json)
+            {
+                if (json == null)
+                {
+                    return null;
+                }
+
+                _stringBuilder ??= new StringBuilder();
+                _stringBuilderRes ??= new StringBuilder();
+                _stringBuilder.Clear();
+
+                for (var i = 0; i < json.Length; i++)
+                {
+                    var c = json[i];
+                    if (c == '"')
+                    {
+                        i = AppendUntilStringEnd(i, json);
+                        continue;
+                    }
+
+                    if (char.IsWhiteSpace(c))
+                    {
+                        continue;
+                    }
+
+                    _stringBuilder.Append(c);
+                }
+
+                var s = _stringBuilder.ToString();
+                _stringBuilder.Clear();
+                ParseAnonymousValue(s);
+                s = _stringBuilderRes.ToString();
+                _stringBuilderRes.Clear();
+                return s;
+            }
+
+            private static int AppendUntilStringEnd(int startIdx, string json, char end = '"')
+            {
+                _stringBuilder.Append(json[startIdx]);
+                for (var i = startIdx + 1; i < json.Length; i++)
+                {
+                    _stringBuilder.Append(json[i]);
+                    if (json[i] == end)
+                    {
+                        return i;
+                    }
+                }
+
+                return json.Length - 1;
+            }
+
+            private static List<string> Split(string json)
+            {
+                var splitArray = new List<string>();
+                if (json.Length == 2)
+                {
+                    return splitArray;
+                }
+
+                var parseDepth = 0;
+                _stringBuilder.Clear();
+                for (var i = 1; i < json.Length - 1; i++)
+                {
+                    switch (json[i])
+                    {
+                        case '[':
+                        case '{':
+                            parseDepth++;
+                            break;
+                        case ']':
+                        case '}':
+                            parseDepth--;
+                            break;
+                        case '"':
+                            i = AppendUntilStringEnd(i, json);
+                            continue;
+                        case ',':
+                        case ':':
+                            if (parseDepth == 0)
+                            {
+                                splitArray.Add(_stringBuilder.ToString());
+                                _stringBuilder.Clear();
+                                continue;
+                            }
+
+                            break;
+                        case 'n' when json[i + 1] == 'e' && json[i + 2] == 'w': // newXXX
+                        case 'O' when json[i + 1] == 'b' && json[i + 2] == 'j': // ObjectId
+                        case 'I' when json[i + 1] == 'S' && json[i + 2] == 'O': // ISODate
+                        case 'N' when json[i + 1] == 'u' && json[i + 2] == 'm': // NumberDecimal / NumberLong
+                        case 'T' when json[i + 1] == 'i' && json[i + 2] == 'm': // Timestamp
+                            i = AppendUntilStringEnd(i, json, ')');
+                            continue;
+                    }
+
+                    _stringBuilder.Append(json[i]);
+                }
+
+                splitArray.Add(_stringBuilder.ToString());
+                _stringBuilder.Clear();
+                return splitArray;
+            }
+
+            private static Tuple<string, bool> ParseAnonymousValue(string json, string name = null)
+            {
+                if (json.Length == 0)
+                {
+                    return new Tuple<string, bool>(null, true);
+                }
+
+                if (json[0] == '{' && json[json.Length - 1] == '}')
+                {
+                    return ProcessObject(json, name);
+                }
+
+                if (json[0] == '[' && json[json.Length - 1] == ']')
+                {
+                    return ProcessArray(json, name);
+                }
+
+                return new Tuple<string, bool>("\"?\"", true);
+            }
+
+            private static Tuple<string, bool> ProcessObject(string json, string name)
+            {
+                var elems = Split(json);
+                if (elems.Count % 2 != 0)
+                {
+                    return new Tuple<string, bool>(null, true);
+                }
+
+                if (name != null)
+                {
+                    _stringBuilderRes.Append(name).Append(":");
+                }
+
+                _stringBuilderRes.Append('{');
+                for (var i = 0; i < elems.Count; i += 2)
+                {
+                    var tuple = ParseAnonymousValue(elems[i + 1], elems[i]);
+                    if (tuple.Item2)
+                    {
+                        _stringBuilderRes.Append(elems[i]).Append(':').Append(tuple.Item1);
+                    }
+
+                    if (i != elems.Count - 2)
+                    {
+                        _stringBuilderRes.Append(',');
+                    }
+                }
+
+                _stringBuilderRes.Append('}');
+                return new Tuple<string, bool>(null, false);
+            }
+
+            private static Tuple<string, bool> ProcessArray(string json, string name)
+            {
+                var items = Split(json);
+                if (name != null)
+                {
+                    _stringBuilderRes.Append(name).Append(":");
+                }
+
+                _stringBuilderRes.Append('[');
+                for (var i = 0; i < items.Count; i++)
+                {
+                    var tuple = ParseAnonymousValue(items[i]);
+                    _stringBuilderRes.Append(tuple.Item1);
+                    if (i != items.Count - 1)
+                    {
+                        _stringBuilderRes.Append(',');
+                    }
+                }
+
+                _stringBuilderRes.Append(']');
+                return new Tuple<string, bool>(null, false);
+            }
         }
     }
 }
